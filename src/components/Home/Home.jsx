bde23a27b659522799d6406fedbe10e8b5d831be
kopala123 <anri.kopaliani.1@btu.edu.ge>
2023-02-06 21:10:59 +0400

    home page created

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import redberryLogo from "../../images/redberryLogo.png";
+import agencyLogo from "../../images/transformationAgencyLogo.png";
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <nav className={styles.navbar}>
+        <img
+          src={redberryLogo}
+          alt="redberry logo"
+          className={styles.redberryLogo}
+        />
+        <div></div>
+      </nav>
+      <div className={styles.buttonContainer}>
+        {/* <Link style={{ textDecoration: "none", color: "#fff" }} to="/1"> */}
+        <button>ᲠᲔᲖᲘᲣᲛᲔᲡ ᲓᲐᲛᲐᲢᲔᲑᲐ</button>
+        {/* </Link> */}
+      </div>
+      <img src={agencyLogo} alt="agency logo" className={styles.agencyLogo} />
+    </div>
+  );
+};
+
+export default Home;
